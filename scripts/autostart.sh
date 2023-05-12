5e9959ab671d29def4a4936c5178af942676b95d
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2023-05-12 09:48:26 +0200

    add scripts and install manual

---
+++ scripts/autostart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PATH=/usr/local/bin:$PATH
+export DISPLAY=:0.0
+sleep 10 # can be lower (5) for rpi3
+jackd -P75 -p16 -dalsa -dhw:0 -r44100 -p1024 -n3
+sclang ~/main.scd
---
+++ scripts/panic.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+killall sclang scsynth
+sleep 10
+bash ~/autostart.sh
